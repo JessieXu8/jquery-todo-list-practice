8dfb36a75475450e2cc87725d672bf79aefb0ca4
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-17 16:46:21 +0800

    [jessie] compelted the function of change status

--- js/todo.js
+++ js/todo.js
@@ -22,23 +22,6 @@ $(document)
         }
 
         // code to be implemented
-        // $("#button").click(function () { // add function
-        //     if ($(".input-text").val()) {
-        //     let $val = $(`<li id="${generateUUID()}" class="">
-        //     <input name="done-todo" type="checkbox" class="done-todo"> ${$(".input-text").val()} </li>`)
-        //     $("ol").append($val);
-        //     } else {
-        //     alert("请填写添加内容");
-        //     }
-        //     })
-
-        // $(document).on("change",".done-todo",function () { //on the change of check
-        //     if ($(this).is(":checked")) {
-        //     $(this).parent().addClass("checked");
-        //     } else {
-        //     $(this).parent().removeClass("checked");
-        //     }
-        //     });
 
         $("#button").click(function(){
             if($(".input-text").val()){
@@ -47,6 +30,14 @@ $(document)
                 $("ol").append($var);
             }else{
                 alert("请填写需要增加的内容！");
+            }
+        })
+
+        $(document).on("change",".done-todo",function(){
+            if($(this).is(":checked")){
+                $(this).parent().addClass("checked");
+            }else{
+                $(this).parent().removeClass("checked");
             }
         })
     });
